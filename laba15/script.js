82b8e623cfc821f7f3c4676476dd7b59129e913a
max <alax20013@gmail.com>
2023-05-12 09:41:56 +0300

    updade

--- laba15/script.js
+++ laba15/script.js
@@ -1,17 +1,19 @@
 let a = 1; 
 console.log(a); 
 alert(a); 
-let b = confirm("Yes?"); 
+let b = confirm("да?"); 
 alert(b); 
-let c = prompt("How old are u?", 15); 
+let c = prompt("Сколько тебе лет?"); 
 console.log(c); 
 document.write(c);
 let d = 0; 
 while(d <=100){ 
     console.log(d); 
     d++;   
-}; 
-let i = 0; 
+}
+
+
+let i = 0
 do{ 
     if(i ===0){ 
         console.log(i + ' - це нуль') 
@@ -24,7 +26,9 @@ do{
     } 
     i++; 
  
-} 
+}
+
+
 while (i <= 10);
 let numb = 10000; 
 let counter = 0; 
@@ -34,5 +38,5 @@ do{
 } 
 while (numb >= 50); 
 let result = numb; 
-console.log("Result:" + result); 
-console.log("Count:" + counter);
+console.log("Result:" + result) 
+console.log("Count:" + counter)*/
